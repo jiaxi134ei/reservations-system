e4b58983d6e074df9f624f0560d0f5643b4f110a
jiaxi134ei <jiabiao_1234@126.com>
2019-06-27 16:11:38 +0800

    Initial commit

---
+++ src/main/java/com/justin/reservationssystem/ReservationsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.justin.reservationssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationsSystemApplication.class, args);
+    }
+
+}
